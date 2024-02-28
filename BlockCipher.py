fdd29ce7065edf2f81602e6cfd2bae046b1305d1
alexe14ernyakov <alexe14ernyakov@mail.ru>
2024-02-28 14:28:35 +0300

    +- working ECB (troubles with output: some trash symbols in the end, but bytes has right length)

--- BlockCipher.py
+++ BlockCipher.py
@@ -1,5 +1,6 @@
 from enum import Enum
 from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad
 
 
 class Mode(Enum):
@@ -11,6 +12,8 @@ class Mode(Enum):
 
 
 class BlockCipher:
+    __BLOCK_SIZE = 16
+
     def __init__(self, key, mode):
         if len(key) != 16:
             raise ValueError("Key length must be 16")
@@ -18,15 +21,13 @@ class BlockCipher:
         self.__key = key
         self.__mode = mode
 
-    @staticmethod
-    def __block_cipher_encrypt(data: bytes) -> bytes:
-        cipher = AES.new(data, AES.MODE_ECB)
-        return cipher.encrypt(data).hex()
+    def __block_cipher_encrypt(self, data: bytes) -> bytes:
+        cipher = AES.new(self.__key, AES.MODE_ECB)
+        return cipher.encrypt(data)
 
-    @staticmethod
-    def __block_cipher_decrypt(data: bytes) -> bytes:
-        cipher = AES.new(data, AES.MODE_ECB)
-        return cipher.decrypt(data).hex()
+    def __block_cipher_decrypt(self, data: bytes) -> bytes:
+        cipher = AES.new(self.__key, AES.MODE_ECB)
+        return cipher.decrypt(data)
 
     def set_key(self, key: bytes):
         if len(key) != 16:
@@ -37,13 +38,51 @@ class BlockCipher:
         self.__mode = mode
 
     def process_block_encrypt(self, data: bytes, is_final_block: bool, padding: str) -> bytes:
-        pass
+        match self.__mode:
+            case Mode.ECB:
+                if is_final_block:
+                    data = pad(data, self.__BLOCK_SIZE)
+
+                return self.__block_cipher_encrypt(data)
+            case Mode.CBC:
+                return b'b'
+            case Mode.CFB:
+                return b'c'
+            case Mode.OFB:
+                return b'd'
+            case Mode.CTR:
+                return b'e'
 
     def process_block_decrypt(self, data: bytes, is_final_block: bool, padding: str) -> bytes:
-        pass
+        match self.__mode:
+            case Mode.ECB:
+                if is_final_block:
+                    data = pad(data, self.__BLOCK_SIZE)
 
-    def encrypt(self, data: bytes, iv: bytes) -> bytes:
-        pass
+                return self.__block_cipher_decrypt(data)
+            case Mode.CBC:
+                return b'b'
+            case Mode.CFB:
+                return b'c'
+            case Mode.OFB:
+                return b'd'
+            case Mode.CTR:
+                return b'e'
 
-    def decrypt(self, data: bytes) -> bytes:
-        pass
+    def encrypt(self, data: bytes, iv: bytes = None) -> bytes:
+        blocks = [data[i:i + self.__BLOCK_SIZE] for i in range(0, len(data), self.__BLOCK_SIZE)]
+
+        ciphertext = b''
+        for block in blocks:
+            ciphertext = ciphertext + self.process_block_encrypt(block, block == blocks[-1], 'PKCS7')
+
+        return ciphertext
+
+    def decrypt(self, data: bytes, iv: bytes = None) -> bytes:
+        blocks = [data[i:i + self.__BLOCK_SIZE] for i in range(0, len(data), self.__BLOCK_SIZE)]
+
+        plaintext = b''
+        for block in blocks:
+            plaintext = plaintext + self.process_block_decrypt(block, block == blocks[-1], 'PKCS7')
+
+        return plaintext
--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-if __name__ == '__main__':
-    pass
+from BlockCipher import BlockCipher, Mode
 
+if __name__ == '__main__':
+    b = BlockCipher(b'sixteen-byte-key', Mode.ECB)
+
+    data = b'cryptography-is-my-favorite-subject-in-university'
+    b.set_key(b'sixteen-byte-key')
+    print(data, len(data))
+
+    enc = b.encrypt(data)
+    print(enc, len(data))
+
+    dec = b.decrypt(enc)
+    print(dec, len(data))
